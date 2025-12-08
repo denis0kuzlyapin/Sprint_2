ff4725cf25e2f0459fe2fb644a6a5fbaf8baa88f
Denis Kuzlyapin <kuzlyapindenis@yandex.ru>
2025-12-08 09:35:40 +0300

    Использовать метод родительского класса в дочернем

--- task_1.py
+++ task_1.py
@@ -19,11 +19,8 @@ class ExtendedCase(Case):
         super().__init__(test_case_id, name, step_description, expected_result)
 
     def print_test_case_info(self):
-        print(f"ID тест-кейса: {self.test_case_id}"
-              f"\nНазвание: {self.name}"
-              f"\nОписание шага: {self.step_description}"
-              f"\nОжидаемый результат: {self.expected_result}"
-              f"\nПредусловие: {self.precondition}"
+        super().print_test_case_info()  # Используем метод из родительского класса Case
+        print(f"\nПредусловие: {self.precondition}"
               f"\nОкружение: {self.environment}")
 
 
